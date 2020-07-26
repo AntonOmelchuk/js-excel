8aeac17af675e794ca0c5ab17ba013fda23a55af
AntonOmelchuk <kino_kg@ukr.net>
2020-07-26 20:26:13 +0300

    remove extra comments

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -11,17 +11,17 @@ export class ExcelComponent extends DomListener {
 
     this.prepare()
   }
-  // configure component before init
+
   prepare() {}
 
   toHtml() {
     return ``
   }
-  // notify subscribers about new event
+
   $emit(event, ...args) {
     this.emitter.emit(event, ...args)
   }
-  // subscribe on event
+
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn)
     this.unsubscribers.push(unsub)
